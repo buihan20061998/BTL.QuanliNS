f24849c64013628ba77a6a57a2a791fc80a9310b
BUI THI HAN <1621050459@student.humg.edu.vn>
2020-10-02 09:54:58 +0700

    Add project files.

---
+++ Models/Nhansu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace BTL.QuanliNS.Models
+{
+    public class Nhansu
+    {
+        [Key]
+        public string MaNS { Get; Set; }
+        public string HoTen { Get; Set; }
+        public string Gioitinh { Get; Set; }
+        public string Quequan { Get; Set; }
+        public string Email { Get; Set; }
+        public string Sodienthoai { Get; Set; }
+
+    }
+}
